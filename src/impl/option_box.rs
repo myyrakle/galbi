1d27ad60bab3a5ccdde17f3c17d147599ddcaeed
myyrakle <sssang97@naver.com>
2020-12-28 13:10:26 +0900

    impl OptionBox::{and, and_then}

--- src/impl/option_box.rs
+++ src/impl/option_box.rs
@@ -13,7 +13,7 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let value = OptionBox::new(Some(10));
     /// ```
     pub fn new(x: Option<T>) -> OptionBox<T> {
@@ -30,12 +30,12 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let value:OptionBox<i32> = OptionBox::some(10);
     /// ```
     pub fn some(x: T) -> OptionBox<T> {
         OptionBox {
-            ptr: Some(Box::new(x))
+            ptr: Some(Box::new(x)),
         }
     }
 
@@ -43,23 +43,21 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let value:OptionBox<u32> = OptionBox::none();
     /// ```
     pub fn none() -> OptionBox<T> {
-        OptionBox {
-            ptr: None
-        }
+        OptionBox { ptr: None }
     }
 
     /// Returns true if the option is a Some value.
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x: OptionBox<u32> = OptionBox::some(19);
     /// assert_eq!(x.is_some(), true);
-    /// 
+    ///
     /// let x: OptionBox<u32> = OptionBox::none();
     /// assert_eq!(x.is_some(), false);
     /// ```
@@ -71,10 +69,10 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x: OptionBox<u32> = OptionBox::some(19);
     /// assert_eq!(x.is_none(), false);
-    /// 
+    ///
     /// let x: OptionBox<u32> = OptionBox::none();
     /// assert_eq!(x.is_none(), true);
     /// ```
@@ -86,7 +84,7 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let text: OptionBox<String> = OptionBox::some("Hello, world!".to_string());
     /// // First, cast `OptionBox<String>` to `Option<&Box<T>>` with `as_ref`,
     /// // then consume *that* with `map`, leaving `text` on the stack.
@@ -101,7 +99,7 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let mut x = OptionBox::some(2);
     /// match x.as_mut() {
     ///    Some(v) => *v = Box::new(42),
@@ -117,12 +115,12 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x = OptionBox::some("foo".to_string());
     /// let value = x.expect("error");
     /// assert_eq!(value, Box::new("foo".to_string()));
     /// ```
-    pub fn expect(self, msg:&str) -> Box<T> {
+    pub fn expect(self, msg: &str) -> Box<T> {
         self.ptr.expect(msg)
     }
 
@@ -131,7 +129,7 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x = OptionBox::some("foo".to_string());
     /// let value = x.unwrap();
     /// assert_eq!(value, Box::new("foo".to_string()));
@@ -145,7 +143,7 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// assert_eq!(OptionBox::some(12).unwrap_or(Box::new(13)), Box::new(12));
     /// assert_eq!(OptionBox::none().unwrap_or(Box::new(13)), Box::new(13));
     /// ```
@@ -157,12 +155,13 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// assert_eq!(OptionBox::some(12).unwrap_or_else(|| Box::new(13)), Box::new(12));
     /// assert_eq!(OptionBox::none().unwrap_or_else(|| Box::new(13)), Box::new(13));
     /// ```
-    pub fn unwrap_or_else<F>(self, f: F) -> Box<T> 
-    where F: FnOnce()->Box<T>
+    pub fn unwrap_or_else<F>(self, f: F) -> Box<T>
+    where
+        F: FnOnce() -> Box<T>,
     {
         self.ptr.unwrap_or_else(f)
     }
@@ -171,94 +170,96 @@ impl<T> OptionBox<T> {
     ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// assert_eq!(OptionBox::some(20).map(|e| (*e)*2), Some(40));
     /// ```
-    pub fn map<U, F>(self, f: F) -> Option<U> 
-    where F: FnOnce(Box<T>)->U
+    pub fn map<U, F>(self, f: F) -> Option<U>
+    where
+        F: FnOnce(Box<T>) -> U,
     {
         self.ptr.map(f)
     }
 
     /// Applies a function to the contained value (if any), or returns the provided default (if not).
     /// Arguments passed to map_or are eagerly evaluated; if you are passing the result of a function call, it is recommended to use map_or_else, which is lazily evaluated.
-    /// 
+    ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x = OptionBox::some(10);
     /// assert_eq!(x.map_or(111, |e| *e), 10);
-    /// 
+    ///
     /// let x: OptionBox<i32> = OptionBox::none();
     /// assert_eq!(x.map_or(10, |e| *e), 10);
     /// ```
-    pub fn map_or<U, F>(self, default:U, f: F) -> U 
-    where F: FnOnce(Box<T>)->U
+    pub fn map_or<U, F>(self, default: U, f: F) -> U
+    where
+        F: FnOnce(Box<T>) -> U,
     {
         self.ptr.map_or(default, f)
     }
 
     /// Applies a function to the contained value (if any), or computes a default (if not).
-    /// 
+    ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x = OptionBox::some(10);
     /// assert_eq!(x.map_or_else(||111, |e| *e), 10);
-    /// 
+    ///
     /// let x: OptionBox<i32> = OptionBox::none();
     /// assert_eq!(x.map_or_else(||10, |e| *e), 10);
     /// ```
-    pub fn map_or_else<U, D, F>(self, default:D, f: F) -> U 
-    where 
-        D: FnOnce()->U,
-        F: FnOnce(Box<T>)->U
+    pub fn map_or_else<U, D, F>(self, default: D, f: F) -> U
+    where
+        D: FnOnce() -> U,
+        F: FnOnce(Box<T>) -> U,
     {
         self.ptr.map_or_else(default, f)
     }
 
     /// Transforms the `Option<Box<T>>` into a `Result<Box<T>, E>`, mapping Some(v) to Ok(v) and None to Err(err).
     /// Arguments passed to ok_or are eagerly evaluated; if you are passing the result of a function call, it is recommended to use ok_or_else, which is lazily evaluated.
-    /// 
+    ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x = OptionBox::some(10);
     /// assert_eq!(x.ok_or(Box::new(0)), Ok(Box::new(10)));
-    /// 
+    ///
     /// let x: OptionBox<i32> = OptionBox::none();
     /// assert_eq!(x.ok_or(Box::new(0)), Err(Box::new(0)));
     /// ```
-    pub fn ok_or<E>(self, err: E) -> Result<Box<T>, E>
-    {
+    pub fn ok_or<E>(self, err: E) -> Result<Box<T>, E> {
         self.ptr.ok_or(err)
     }
 
     /// Transforms the Option<Box<T>> into a Result<Box<T>, E>, mapping Some(v) to Ok(v) and None to Err(err()).
-    /// 
+    ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x = OptionBox::some(10);
     /// assert_eq!(x.ok_or_else(||Box::new(0)), Ok(Box::new(10)));
-    /// 
+    ///
     /// let x: OptionBox<i32> = OptionBox::none();
     /// assert_eq!(x.ok_or_else(||Box::new(0)), Err(Box::new(0)));
     /// ```
     pub fn ok_or_else<E, F>(self, err: F) -> Result<Box<T>, E>
     where
-        F: FnOnce() -> E, {
+        F: FnOnce() -> E,
+    {
         self.ptr.ok_or_else(err)
     }
 
     /// Returns an iterator over the possibly contained value.
-    /// 
+    ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let x = OptionBox::some(10);
     /// assert_eq!(x.iter().next(), Some(&Box::new(10)));
-    /// 
+    ///
     /// let x: OptionBox<i32> = OptionBox::none();
     /// assert_eq!(x.iter().next(), None);
     /// ```
@@ -267,21 +268,70 @@ impl<T> OptionBox<T> {
     }
 
     /// Returns a mutable iterator over the possibly contained value.
-    /// 
+    ///
     /// ```rust
     /// use galbi::*;
-    /// 
+    ///
     /// let mut x = OptionBox::some(4);
     /// match x.iter_mut().next() {
     ///    Some(v) => *v = Box::new(42),
     ///    None => {},
     /// }
     /// assert_eq!(x, OptionBox::some(42));
-    /// 
+    ///
     /// let mut x: OptionBox<u32> = OptionBox::none();
     /// assert_eq!(x.iter_mut().next(), None);
     /// ```
     pub fn iter_mut(&mut self) -> IterMut<'_, Box<T>> {
         self.ptr.iter_mut()
+    }
+
+    /// Returns None if the option is None, otherwise returns optb.
+    ///
+    /// ```rust
+    /// use galbi::*;
+    ///
+    /// let x = OptionBox::some(2);
+    /// let y: Option<String> = None;
+    /// assert_eq!(x.and(y), None);
+    ///
+    /// let x: OptionBox<i32> = OptionBox::none();
+    /// let y = Some("foo".to_string());
+    /// assert_eq!(x.and(y), None);
+    ///
+    /// let x = OptionBox::some(2);
+    /// let y = Some("foo".to_string());
+    /// assert_eq!(x.and(y), Some("foo".to_string()));
+    ///
+    /// let x: OptionBox<i32> = OptionBox::none();
+    /// let y: Option<String> = None;
+    /// assert_eq!(x.and(y), None);
+    /// ```
+    pub fn and<U>(self, optb: Option<U>) -> Option<U> {
+        self.ptr.and(optb)
+    }
+
+    /// Returns None if the option is None, otherwise calls f with the wrapped value and returns the result.
+    /// Some languages call this operation flatmap.
+    ///
+    /// ```rust
+    /// use galbi::*;
+    ///
+    /// fn sq(x: Box<u32>) -> Option<u32> {
+    ///     Some((*x)*(*x))
+    /// }
+    /// fn nope(x: Box<u32>) -> Option<u32> {
+    ///     None
+    /// }
+    ///
+    /// assert_eq!(OptionBox::some(2).and_then(sq), Some(4));
+    /// assert_eq!(OptionBox::some(2).and_then(nope), None);
+    /// assert_eq!(OptionBox::<u32>::none().and_then(sq), None);
+    /// ```
+    pub fn and_then<U, F>(self, f: F) -> Option<U>
+    where
+        F: FnOnce(Box<T>) -> Option<U>,
+    {
+        self.ptr.and_then(f)
     }
 }
